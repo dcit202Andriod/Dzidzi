387c2ab1de3bcb217bfe9bf3d91b70a87df91317
willyjo1 <arthurwilchield@gmail.com>
2023-07-04 20:56:50 +0000

    category sections created

--- app/src/main/java/com/example/dzidzi/Adapters/HomeAdapter.java
+++ app/src/main/java/com/example/dzidzi/Adapters/HomeAdapter.java
@@ -1,37 +1,64 @@
 package com.example.dzidzi.Adapters;
 
+import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.dzidzi.Models.RecipeCategory;
+import com.example.dzidzi.R;
 
 import java.util.ArrayList;
 
-public class HomeAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
-    private ArrayList<RecipeCategory> recipeCategories = new ArrayList<>();
+public class HomeAdapter extends RecyclerView.Adapter<HomeAdapter.ViewHolder>{
+    Context context;
+
+    ArrayList<RecipeCategory> homeArr = new ArrayList<>();
+
+    public void setHomeArr(ArrayList<RecipeCategory> homeArr) {
+        this.homeArr = homeArr;
+        notifyDataSetChanged();
+    }
+
+    public HomeAdapter(Context context) {
+        this.context = context;
+    }
+
+
+
+
+
     @NonNull
     @Override
-    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.category_layout, parent, false);
+        ViewHolder holder = new ViewHolder(view);
+        return holder;
     }
 
     @Override
-    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
-
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        holder.txtName.setText(homeArr.get(position).getCategory());
+        holder.imgName.setImageResource(homeArr.get(position).getId());
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return homeArr.size();
     }
 
-    public  class ViewHolder extends RecyclerView.ViewHolder{
-
+    public class ViewHolder extends RecyclerView.ViewHolder{
+        private TextView txtName;
+        private ImageView imgName;
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
+            txtName = itemView.findViewById(R.id.categoryName);
+            imgName = itemView.findViewById(R.id.categoryImg);
         }
     }
 }
--- app/src/main/java/com/example/dzidzi/MainActivity.java
+++ app/src/main/java/com/example/dzidzi/MainActivity.java
@@ -1,12 +1,14 @@
 package com.example.dzidzi;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.widget.Toast;
 
+import com.example.dzidzi.Adapters.HomeAdapter;
 import com.example.dzidzi.Database.RecipeDatabaseHelper;
-import com.example.dzidzi.Models.Recipe;
 import com.example.dzidzi.Models.RecipeCategory;
 
 import java.util.ArrayList;
@@ -14,16 +16,26 @@ import java.util.ArrayList;
 public class MainActivity extends AppCompatActivity {
     RecipeDatabaseHelper homeDatabaseHelper = new RecipeDatabaseHelper(this);
     ArrayList<RecipeCategory> homeArr = new ArrayList<>();
+    private RecyclerView itemRecyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        itemRecyclerView = findViewById(R.id.recParent);
 
         //Array to get all categories ie. breakfast, lunch and supper and their ids
         //Test
         //Test2
         //Test3
         homeArr = homeDatabaseHelper.getAllCategories();
+        HomeAdapter homeAdapter = new HomeAdapter(this);
+        homeAdapter.setHomeArr(homeArr);
+
+        itemRecyclerView.setAdapter(homeAdapter);
+        itemRecyclerView.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false));
+
+        Toast.makeText(this, homeArr.get(0).getId() +" " + homeArr.get(0).getCategory(), Toast.LENGTH_SHORT).show();
+
     }
 }
