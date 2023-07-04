6dae49ac58283de9d929dfaa6acbf13f6b33bcb9
Aamina <yaawsley@gmail.com>
2023-07-04 22:46:15 +0000

    Scrollview and menu changes made

--- app/src/main/java/com/example/dzidzi/Recipe.java
+++ app/src/main/java/com/example/dzidzi/Recipe.java
@@ -3,12 +3,32 @@ package com.example.dzidzi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.PopupMenu;
+import android.widget.Toast;
 
-public class Recipe extends AppCompatActivity {
-
+public class Recipe extends AppCompatActivity implements PopupMenu.OnMenuItemClickListener {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recipe);
+    }
+
+    public void showPopup(View v) {
+        PopupMenu popup = new PopupMenu(Recipe.this, v);
+        popup.setOnMenuItemClickListener(Recipe.this);
+        popup.getMenuInflater().inflate(R.menu.list_menu, popup.getMenu());
+
+    }
+
+    @Override
+    public boolean onMenuItemClick(MenuItem item) {
+        if (item.getItemId() == R.id.item1){
+            Toast.makeText(this, "Item 1 clicked", Toast.LENGTH_SHORT).show();
+            return true;
+        }
+        return true;
     }
 }
