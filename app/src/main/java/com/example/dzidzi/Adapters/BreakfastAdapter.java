c156d47766dc9e2d24260d18289d75029876c667
NanaOdoom <yaawsley@gmail.com>
2023-07-04 18:31:12 +0000

    Breakfast recyclerview done

--- app/src/main/java/com/example/dzidzi/Adapters/BreakfastAdapter.java
+++ app/src/main/java/com/example/dzidzi/Adapters/BreakfastAdapter.java
@@ -1,4 +1,62 @@
 package com.example.dzidzi.Adapters;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-public class BreakfastAdapter {
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.dzidzi.Breakfast;
+import com.example.dzidzi.Models.Recipe;
+import com.example.dzidzi.R;
+
+import java.util.ArrayList;
+import java.util.zip.Inflater;
+
+public class BreakfastAdapter extends RecyclerView.Adapter<BreakfastAdapter.MyViewHolder> {
+
+    Context context;
+    private ArrayList<Recipe> breakfastArr = new ArrayList<>();
+
+    public BreakfastAdapter(Context context, ArrayList<Recipe> breakfastArr) {
+        this.context = context;
+        this.breakfastArr = breakfastArr;
+    }
+
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater inflater = LayoutInflater.from(context);
+        View view = inflater.inflate(R.layout.breakfastview, parent, false);
+
+        return new BreakfastAdapter.MyViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+
+        holder.imageView.setImageResource(breakfastArr.get(position).getImg());
+        holder.textView.setText(breakfastArr.get(position).getName());
+    }
+
+    @Override
+    public int getItemCount() {
+        return breakfastArr.size();
+    }
+
+    public class MyViewHolder extends RecyclerView.ViewHolder{
+
+
+        ImageView imageView;
+        TextView textView;
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            imageView = itemView.findViewById(R.id.imageView);
+            textView = itemView.findViewById(R.id.textView);
+        }
+    }
 }
--- app/src/main/java/com/example/dzidzi/Breakfast.java
+++ app/src/main/java/com/example/dzidzi/Breakfast.java
@@ -1,16 +1,22 @@
 package com.example.dzidzi;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.GridLayout;
 import android.widget.Toast;
 
+import com.example.dzidzi.Adapters.BreakfastAdapter;
 import com.example.dzidzi.Database.RecipeDatabaseHelper;
 import com.example.dzidzi.Models.Recipe;
 
 import java.util.ArrayList;
 
 public class Breakfast extends AppCompatActivity {
+
+
     RecipeDatabaseHelper breakfastDatabaseHelper = new RecipeDatabaseHelper(this);
     ArrayList<Recipe> breakfastArr = new ArrayList<>();
 
@@ -19,8 +25,21 @@ public class Breakfast extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_breakfast);
 
+        RecyclerView recyclerView = findViewById(R.id.recyclerview);
+
+        BreakfastAdapter adapter = new BreakfastAdapter(this, breakfastArr);
+
+
+
+
         //Breakfast array from database.
         breakfastArr = breakfastDatabaseHelper.getBreakfastRecipes();
+
+        GridLayoutManager gridLayoutManager = new GridLayoutManager(this, 2,GridLayoutManager.VERTICAL,false);
+        recyclerView.setLayoutManager(gridLayoutManager);
+        recyclerView.setAdapter(adapter);
+
+
 
     }
 }
