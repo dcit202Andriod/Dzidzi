d2523e2ee21567b394aecaf2df99f46dc02a5e19
willyjo1 <arthurwilchield@gmail.com>
2023-07-04 01:22:20 +0000

    worked on the recycler view for the home page

--- app/src/main/java/com/example/dzidzi/Adapters/HomeAdapter.java
+++ app/src/main/java/com/example/dzidzi/Adapters/HomeAdapter.java
@@ -1,4 +1,37 @@
 package com.example.dzidzi.Adapters;
 
-public class HomeAdapter {
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.dzidzi.Models.RecipeCategory;
+
+import java.util.ArrayList;
+
+public class HomeAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+    private ArrayList<RecipeCategory> recipeCategories = new ArrayList<>();
+    @NonNull
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public  class ViewHolder extends RecyclerView.ViewHolder{
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
 }
