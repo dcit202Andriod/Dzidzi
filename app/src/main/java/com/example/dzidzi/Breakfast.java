472126b53190fa485b7b3a9a73d67944b5db8e6f
= <=>
2023-07-08 00:33:34 -1200

    Menu button and logo routes created

--- app/src/main/java/com/example/dzidzi/Breakfast.java
+++ app/src/main/java/com/example/dzidzi/Breakfast.java
@@ -4,8 +4,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.PopupMenu;
+import android.widget.TextView;
 
 import com.example.dzidzi.Adapters.BreakfastAdapter;
 import com.example.dzidzi.Database.RecipeDatabaseHelper;
@@ -13,16 +18,28 @@ import com.example.dzidzi.Models.Recipe;
 
 import java.util.ArrayList;
 
-public class Breakfast extends AppCompatActivity{
+public class Breakfast extends AppCompatActivity implements PopupMenu.OnMenuItemClickListener {
 
 
     RecipeDatabaseHelper breakfastDatabaseHelper = new RecipeDatabaseHelper(this);
     ArrayList<Recipe> breakfastArr = new ArrayList<>();
+    TextView breakfastHeader;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_breakfast);
+
+        breakfastHeader = findViewById(R.id.breakfastHeader);
+
+        breakfastHeader.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Context context = Breakfast.this;
+                Intent i = new Intent(context, MainActivity.class);
+                context.startActivity(i);
+            }
+        });
 
         RecyclerView recyclerView = findViewById(R.id.recyclerview);
 
@@ -37,6 +54,27 @@ public class Breakfast extends AppCompatActivity{
 
 
 
+    }
+
+    public void showBreakfastPopup(View v) {
+        PopupMenu popup = new PopupMenu(Breakfast.this, v);
+        popup.setOnMenuItemClickListener(Breakfast.this);
+        popup.getMenuInflater().inflate(R.menu.breakfast_list_menu, popup.getMenu());
+        popup.show();
+    }
+
+    @Override
+    public boolean onMenuItemClick(MenuItem item) {
+        if (item.getItemId() == R.id.item2){
+            Intent i = new Intent(Breakfast.this, Lunch.class);
+            Breakfast.this.startActivity(i);
+            return true;
+        } else if (item.getItemId() == R.id.item3){
+            Intent i = new Intent(Breakfast.this, Supper.class);
+            Breakfast.this.startActivity(i);
+            return true;
+        }
+        return true;
     }
 
 }
--- app/src/main/java/com/example/dzidzi/Supper.java
+++ app/src/main/java/com/example/dzidzi/Supper.java
@@ -4,9 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
 import android.widget.GridLayout;
+import android.widget.PopupMenu;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.dzidzi.Adapters.BreakfastAdapter;
@@ -16,16 +21,28 @@ import com.example.dzidzi.Models.Recipe;
 
 import java.util.ArrayList;
 
-public class Supper extends AppCompatActivity{
+public class Supper extends AppCompatActivity implements PopupMenu.OnMenuItemClickListener{
 
     RecyclerView supperRecView;
     RecipeDatabaseHelper supperDatabaseHelper = new RecipeDatabaseHelper(this);
     ArrayList<Recipe> supperArr = new ArrayList<>();
+    TextView supperHeader;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_supper);
+
+        supperHeader = findViewById(R.id.supperHeader);
+
+        supperHeader.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Context context = Supper.this;
+                Intent i = new Intent(context, MainActivity.class);
+                context.startActivity(i);
+            }
+        });
 
         supperRecView = findViewById(R.id.supperrecview);
 
@@ -40,5 +57,26 @@ public class Supper extends AppCompatActivity{
 
 
 
+    }
+
+    public void showSupperPopup(View v) {
+        PopupMenu popup = new PopupMenu(Supper.this, v);
+        popup.setOnMenuItemClickListener(Supper.this);
+        popup.getMenuInflater().inflate(R.menu.supper_list_menu, popup.getMenu());
+        popup.show();
+    }
+
+    @Override
+    public boolean onMenuItemClick(MenuItem item) {
+        if (item.getItemId() == R.id.item1){
+            Intent i = new Intent(Supper.this, Breakfast.class);
+            Supper.this.startActivity(i);
+            return true;
+        } else if (item.getItemId() == R.id.item2){
+            Intent i = new Intent(Supper.this, Lunch.class);
+            Supper.this.startActivity(i);
+            return true;
+        }
+        return true;
     }
 }
